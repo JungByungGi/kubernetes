2a2e7c210c13e8f87619807d1e1c2d3e9aaf46c3
jbg <jbg@sngy.io>
2021-10-11 00:29:50 +0900

    docker build test

---
+++ Docker/build_test/test_server.py
@@ -0,0 +1,18 @@
+import socket
+
+with socket.socket() as s:
+    s.bind(("0.0.0.0", 12345))
+    s.listen()
+    print("server is started")
+
+    conn, addr = s.accept()
+    # conn 클라이언트와 통신할 소켓
+    # addr 클라이언트의 정보
+
+    with conn:
+        print("Connected by", addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
